197715176afe5e0c6c9183007191d4424615d00b
L-caleb <lewechicaleb@gmail.com>
2022-07-01 05:29:55 -0700

    corrected the output

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,23 +1,40 @@
 /*
- * File: 4-print_alphabt.c
+ * File: 101-print_comb4.c
  * Auth: Brennan D Baraban
  */
 
 #include <stdio.h>
 
 /**
- * main - Prints the alphabet in lowercase, except for q and e.
+ * main - Prints all possible combinations of three different digits,
+ *        in ascending order, separated by a comma followed by a space.
  *
  * Return: Always 0.
  */
 int main(void)
 {
-	char letter;
+	int digit1, digit2, digit3;
 
-	for (letter = 'a'; letter <= 'z'; letter++)
+	for (digit1 = 0; digit1 < 8; digit1++)
 	{
-		if (letter != 'e' && letter != 'q')
-			putchar(letter);
+		for (digit2 = digit1 + 1; digit2 < 9; digit2++)
+		{
+			for (digit3 = digit2 + 1; digit3 < 10; digit3++)
+			{
+				putchar((digit1 % 10) + '0');
+				putchar((digit2 % 10) + '0');
+				putchar((digit3 % 10) + '0');
+
+				if (digit1 == 7 && digit2 == 8 && digit3 == 9)
+					continue;
+				
+				putchar(',');
+				putchar(' ');
+			}
+		}
 	}
 
+	putchar('\n');
 
+	return (0);
+}
