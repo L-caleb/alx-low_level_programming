c3500055a25a188339a5408fb21182fc7d7f130b
L-caleb <lewechicaleb@gmail.com>
2022-07-01 06:12:13 -0700

    correct output

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -11,7 +11,6 @@
  *
  * Return: Always 0.
  */
-
 int main(void)
 {
 	int digit1, digit2, digit3;
@@ -25,13 +24,17 @@ int main(void)
 				putchar((digit1 % 10) + '0');
 				putchar((digit2 % 10) + '0');
 				putchar((digit3 % 10) + '0');
+
 				if (digit1 == 7 && digit2 == 8 && digit3 == 9)
-					   continue;			
-			        putchar(',');
+					continue;
+				
+				putchar(',');
 				putchar(' ');
 			}
 		}
 	}
+
 	putchar('\n');
+
 	return (0);
 }
